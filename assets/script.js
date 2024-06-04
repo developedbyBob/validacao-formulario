39d10503603b096aa2264618153ea0f3acba1c70
Bob <esdrassantos41@gmail.com>
2024-06-03 21:56:42 -0300

    v2 do projeto

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function validateForm() {
+/*unction validateForm() {
   let fields = ["name", "email", "password", "confirm-password"];
   let errorMessage = document.getElementById("error-message");
   errorMessage.innerHTML = "";
@@ -10,6 +10,7 @@ function validateForm() {
       return false;
     }
   }
+
 
   let email = document.getElementById("email").value;
   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,4 +27,62 @@ function validateForm() {
   }
 
   return true;
+}*/
+
+
+class ValidadorFormulario {
+  constructor(name, email, password, confirmPassword) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.confirmPassword = confirmPassword;
+  }
+
+  validateName() {
+    if(this.name.trim() === "") {
+      alert("Porfavor, insira seu nome")
+      return false;
+    }
+
+    return true
+  }
+
+  validateEmail() {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!regexEmail.test(this.email)) {
+      alert("Por favor, insira um email válido")
+      return false;
+    }
+    return true;
+  }
+
+  validatePassword() {
+    if(this.password.length < 8) {
+      alert("A senha deve ter pelo menos 8 caracteres.");
+      return false
+    }
+    return true
+  }
+
+  validateConfirmPassword() {
+    if(this.password !== this.confirmPassword){
+      alert("As senhas não coincidem")
+      return false;
+    }
+    return true
+  }
+
+  validateForm(){
+    return this.validateName() && this.validateEmail() && this.validatePassword() && this.validateConfirmPassword()  
+  }
+}
+
+function validateForm(){
+  const name = document.getElementById("name").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+  const confirmPassword = document.getElementById("confirm-password").value
+
+  const validador = new ValidadorFormulario(name,email,password,confirmPassword)
+  return validador.validateForm()
 }
