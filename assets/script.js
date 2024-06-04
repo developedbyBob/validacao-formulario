80d3a6f77afe1b9ce2f7578534e33a8dcfb8741a
Bob <esdrassantos41@gmail.com>
2024-06-03 21:58:31 -0300

    atualizei

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,3 @@
-/*unction validateForm() {
-  let fields = ["name", "email", "password", "confirm-password"];
-  let errorMessage = document.getElementById("error-message");
-  errorMessage.innerHTML = "";
-
-  for (let i = 0; i < fields.length; i++) {
-    let fieldValue = document.getElementById(fields[i]).value;
-    if (fieldValue === "") {
-      errorMessage.innerHTML = "Por favor, preencha todos os campos.";
-      return false;
-    }
-  }
-
-
-  let email = document.getElementById("email").value;
-  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-    errorMessage.innerHTML = "Por favor, insira um e-mail válido.";
-    return false;
-  }
-
-  let password = document.getElementById('password').value;
-  let confirmPassword = document.getElementById('confirm-password').value;
-  if (password !== confirmPassword) {
-    errorMessage.innerHTML = 'As senhas não coincidem.'
-    return false
-  }
-
-  return true;
-}*/
-
 
 class ValidadorFormulario {
   constructor(name, email, password, confirmPassword) {
